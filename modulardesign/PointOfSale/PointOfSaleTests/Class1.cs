ca5160a350fdf9418b9c692f24b7f0c377280d5f
Xerxes Battiwalla <xerxesb@gmail.com>
2010-12-08 15:49:57 +1100

    Refactored to extract catalog and view

--- modulardesign/PointOfSale/PointOfSaleTests/Class1.cs
+++ modulardesign/PointOfSale/PointOfSaleTests/Class1.cs
@@ -30,38 +30,100 @@ namespace PointOfSaleTests
         }
     }
 
-    public class SaleController
+    public interface ICatalog
     {
-        private readonly IConsoleOutput _consoleOutput;
+        bool ContainsBarcode(string barcode);
+        string FindPrice(string barcode);
+    }
+
+    public class Catalog : ICatalog
+    {
         private readonly IDictionary<string, string> _pricesByProduct;
 
-        public SaleController(IBarcodeReader barcodeReader, IConsoleOutput consoleOutput, IDictionary<string, string> pricesByProduct)
+        public Catalog(IDictionary<string, string> pricesByProduct)
+        {
+            _pricesByProduct = pricesByProduct;
+        }
+
+        public bool ContainsBarcode(string barcode)
+        {
+            return _pricesByProduct.ContainsKey(barcode);
+        }
+
+        public string FindPrice(string barcode)
+        {
+            return _pricesByProduct[barcode];
+        }
+
+    }
+
+    public interface ISaleView
+    {
+        void DisplayPrice(string price);
+        void DisplayProductNotFound(string barcode);
+        void DisplayNoBarcodeProvided();
+    }
+
+    public class SaleView : ISaleView
+    {
+        private readonly IConsoleOutput _consoleOutput;
+
+        public SaleView(IConsoleOutput consoleOutput)
         {
             _consoleOutput = consoleOutput;
-            _pricesByProduct = pricesByProduct;
-            barcodeReader.OnBarcode += HandleBarcode;
+        }
 
-            //if (pricesByProduct == null)
-            //{
-            //    throw new ArgumentNullException("pricesByProduct");
-            //}
+        public void DisplayPrice(string price)
+        {
+            _consoleOutput.WriteLine(price);
+        }
+
+        public void DisplayProductNotFound(string barcode)
+        {
+            _consoleOutput.WriteLine("Product code " + barcode + " not found");
+        }
+
+        public void DisplayNoBarcodeProvided()
+        {
+            _consoleOutput.WriteLine("No barcode was provided");
+        }
+
+    }
+
+    public class SaleController
+    {
+        private readonly ISaleView _saleView;
+        private readonly ICatalog _catalog;
+
+        public SaleController(IBarcodeReader barcodeReader, IConsoleOutput consoleOutput,
+                              IDictionary<string, string> pricesByProduct) : this(barcodeReader, new SaleView(consoleOutput), new Catalog(pricesByProduct))
+        {
+        }
+
+        public SaleController(IBarcodeReader barcodeReader, ISaleView saleView, ICatalog catalog)
+        {
+            _saleView = saleView;
+            _catalog = catalog;
+            barcodeReader.OnBarcode += HandleBarcode;
         }
 
         private void HandleBarcode(string barcode)
         {
+            // this refactor means pulling pout a higher class to perform validation and another controller would use these two classes as collaborators. 
+            // the salecontroller doesnt know anything about validation.
             if (String.IsNullOrEmpty(barcode))
             {
-                _consoleOutput.WriteLine("No barcode was provided");
+                _saleView.DisplayNoBarcodeProvided();
                 return;
             }
 
-            if (!_pricesByProduct.ContainsKey(barcode))
+            if (!_catalog.ContainsBarcode(barcode))
             {
-                _consoleOutput.WriteLine("Product code " + barcode + " not found");
+                _saleView.DisplayProductNotFound(barcode);
                 return;
             }
 
-            _consoleOutput.WriteLine(_pricesByProduct[barcode]);
+            _saleView.DisplayPrice(_catalog.FindPrice(barcode));
         }
     }
 
@@ -116,7 +178,7 @@ namespace PointOfSaleTests
             var barcodeReader = Substitute.For<IBarcodeReader>();
             var consoleOutput = Substitute.For<IConsoleOutput>();
 
-            new SaleController(barcodeReader, consoleOutput, null);
+            new SaleController(barcodeReader, consoleOutput, (IDictionary<string, string>)null);
 
             barcodeReader.OnBarcode += Raise.Action("");
 
